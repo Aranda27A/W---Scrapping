faa98957a27f129720266e4f62fe638b078e0f6d
Abraham Aranda <abraham.arandal@alumno.buap.mx>
2025-04-29 18:48:39 +0300

    Advance on scrapping content by url

--- Scraping-pages/index.js
+++ Scraping-pages/index.js
@@ -1,31 +1,74 @@
-import axios from "axios";
+import axios, { all } from "axios";
 import { load } from "cheerio";
+import { randomBytes } from "crypto";
 import fs from "fs"
+import { text } from "stream/consumers";
 
 
-async function Main() {
+async function JobsScrappersFromPages() {
 
     try{
+        const AllJobs = []
+        for (let i = 1; i < 14; i++) {
+    
+            
 
-        const Extract = await axios.get("https://braigslist.vercel.app/jobs/1/")
+        const Extract = await axios.get(`https://braigslist.vercel.app/jobs/${i}/`)
         const html = Extract.data
 
         fs.writeFileSync("./extract.html" ,  html)
-     
-       
         const $ = load(html)
-        $(".title-blob").each((Number, Element)=> {
+        
+        // $(".title-blob > a").each((Number, Element)=> {
            
-           const Titulo = ($(Element).text())
-            const json = {Number, Titulo}
-            console.log(json);
-            
+        //    const Titulo = $(Element).text()
+        //    const url = $(Element).attr("href")
+        //    const json = {Number, Titulo, url}
+        //    console.log(json);
+        // })
 
-        })
+        //Ahora con .map para ver esa funcionalidad.
+        
+        const json = $(".title-blob > a").map((Number, Element)=> {
+           
+            const Titulo = $(Element).text()
+            const url = $(Element).attr("href")
+             const content = Content(url)
+            return {Number, Titulo, url,content}
+
+         }).get();
+
+         AllJobs.push(...json)
+    
+        };
+
+      console.log(AllJobs); 
+        return AllJobs;
 
     }catch(error){
         console.log( "Error to find" , error);
     };
     
 }
-Main()
+
+async function Content(job) {
+
+  
+    const jobDescription = await axios.get("https://braigslist.vercel.app/" + job[1].url) 
+    const $  = load(jobDescription.data)
+    const Text =  $("div").text()
+    return Text
+
+
+     
+}
+
+
+async function Main() {
+    const AllJobs = await JobsScrappersFromPages()
+    return console.log(AllJobs);
+
+    
+}
+
+JobsScrappersFromPages()

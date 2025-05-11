8185ac7d975f62acaf4a0b84e3f7e1d531f299b5
Abraham Aranda <abraham.arandal@alumno.buap.mx>
2025-05-11 16:45:50 +0300

    Puppeter

---
+++ Puppeteer/index.js
@@ -0,0 +1,43 @@
+import fs from "fs"
+import { url } from "inspector";
+import { title } from "process";
+import puppeteer from "puppeteer";
+
+
+async function Main () { 
+   
+        const browser =  await puppeteer.launch({headless: false});
+        const page = await browser.newPage();
+        await page.goto("https://newyork.craigslist.org/search/tch#search=2~thumb~0")
+      
+        const html = await page.content();
+        fs.writeFileSync("./extract.html" , html)
+        
+        //Important noticing that needs to wait until finds the selector
+        await page.waitForSelector("a.posting-title");
+
+       // Extract titeles in a array
+   const listings = await page.$$eval("a.posting-title", elements =>
+            elements.map(el => ({
+              title: el.innerText.trim(), 
+              url: el.href
+            
+            }))
+          );
+
+  // Show list of titles
+  listings.forEach((post, i) => {
+    console.log(`${i + 1}. ${post.title} `);
+  });
+  //Show The objects 
+  console.log(listings);
+           
+      };
+    
+
+    
+   
+    
+
+
+Main()
